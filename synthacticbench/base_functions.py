f9ac5ea2e18490e3aa0f9e540f63234b396d40e3
Valentin Margraf <valentin.margraf@lmu.de>
2025-01-20 16:15:51 +0100

    changed initialization of Griewank to not hit the optimum in the beginning

--- synthacticbench/base_functions.py
+++ synthacticbench/base_functions.py
@@ -26,7 +26,7 @@ class Griewank(AbstractFunction):
             {
                 f"x_{i}": Float(
                     bounds=(self.lower_bounds[i], self.upper_bounds[i]),
-                    default=0,
+                    default=1,
                     name=f"x_{i}",
                 )
                 for i in range(self.dim)
